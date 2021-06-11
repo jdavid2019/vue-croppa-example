2bf8ef2d3d4efff2032064a1ced74d4c20e1633b
Joseph David Ninaquispe Florez <54923488+jdavid2019@users.noreply.github.com>
2021-06-11 12:22:19 -0500

    recorte automatico con botones

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <div id="app">
+ <div id="app">
+
+   <div class="wrapper">
+     <croppa :height="resizableH"
+             :width="resizableW"
+             v-model="croppa"
+             prevent-white-space
+             class="resizable-croppa"></croppa>
+     <img src="https://i.ibb.co/ccRyVJ3/icons8-plus-math-100.png"
+          class="icon-resize"
+          @mousedown.stop.prevent="onResizeTouchStart">
+   </div>
+
+   <button class="button btn-primary btn-mini-save" @click="croppa.moveLeftwards(10)" style="margin-right: 20px">
+     <img src="https://i.ibb.co/TKfKh32/icons8-button-48.png"  width="32" height="32" alt="">
+     <span>Izquierda</span>
+   </button>
+   <button class="button btn-primary btn-mini-save" @click="croppa.zoomIn()" style="margin-right: 20px">
+     <img src="https://i.ibb.co/TKfKh32/icons8-button-48.png"  width="35" height="35" alt="">
+     <span>Acercar</span>
+   </button>
+   <button class="button btn-primary btn-mini-save" @click="croppa.zoomOut()" style="margin-right: 20px">
+     <img src="https://i.ibb.co/TKfKh32/icons8-button-48.png"  width="35" height="35" alt="">
+     <span>Alejar</span>
+   </button>
+   <button class="button btn-primary btn-mini-save"  @click="croppa.moveRightwards(10)"  style="margin-right: 20px">
+     <img src="https://i.ibb.co/0rQ7J7r/icons8-end-button-48.png"   width="35" height="35" alt="">
+     <span>Derecha</span>
+   </button>
+   <button @click="croppa.flipX()">Rotación espejo</button>
+   <button @click="croppa.rotate()">Rotación</button>
+   <!--   Como te llama? <br>
+      <input type="text" v-model="nombre"> {{ mensaje }} -->
    <!-- <a href="https://zhanziyang.github.io/vue-croppa/#/file-output">D O C U M E N T A C I O N</a>
     <croppa
         v-model="myCroppa"
@@ -70,10 +102,10 @@
 
     <croppa v-model="myCroppa"></croppa> -->
 
-    <croppa v-model="croppa">
+  <!--  <croppa v-model="croppa">
       <img slot="placeholder" crossorigin="anonymous" src="https://i.ibb.co/fHS3PZX/marco.png" class="addon">
     </croppa>
-    <button @click="upload('image/jpeg', 0.8)">UPLOAD</button>
+    <button @click="upload('image/jpeg', 0.8)">UPLOAD</button> -->
 
 
 
@@ -90,14 +122,68 @@ export default {
   },
   data() {
     return {
+      nombre:"",
+      mensaje:"",
       myCroppa: {},
       croppa: {},
       dataUrl: '',
       name: 'ssss',
+//
+      resizing: false,
+      resizableW: 200,
+      resizableH: 200
+    }
+  },
+  mounted () {
+    document.documentElement.addEventListener('mousemove', (evt) => {
+      evt.preventDefault()
+      this.onResizeTouchMove(evt)
+     // console.log(this.onResizeTouchMove(evt))
+    })
+    document.documentElement.addEventListener('mouseup', (evt) => {
+      evt.preventDefault()
+      this.onResizeTouchEnd(evt)
+     // console.log(this.onResizeTouchEnd(evt))
+    })
+  },
+  watch: {
+    nombre:function (value){
+      console.log(value);
+      this.mensaje= "hi everyone";
+      if(value == "oscar"){
+        this.mensaje="se que eres "+value;
+      }
     }
   },
   methods:{
-    upload(type, compressionRate) {
+    /* eslint-disable */
+    onResizeTouchStart (evt) {
+      this.resizing = true
+    },
+    /* eslint-disable */
+    onResizeTouchMove (evt) {
+      if (!this.resizing) return
+      document.documentElement.style.cursor = 'nwse-resize'
+      var croppa = document.querySelector('.resizable-croppa')
+      this.resizableW = evt.clientX - croppa.getBoundingClientRect().left
+      this.resizableH = evt.clientY - croppa.getBoundingClientRect().top
+    },
+
+    /* eslint-disable */
+    onResizeTouchEnd (evt) {
+      this.resizing = false
+      this.croppa.generateBlob((blob) => {
+        let url = URL.createObjectURL(blob)
+        let fd = new FormData()
+        fd.append('file', blob, this.name +'.jpg')
+        let mas = fd.getAll('file');
+        console.log(url)
+        console.log(mas[0])
+      }, 'image/jpeg', 0.8)
+      document.documentElement.style.cursor = 'default'
+    }
+  },
+  save(type, compressionRate) {
       if (!this.croppa.hasImage()) {
         alert('no image to upload')
         return
@@ -178,7 +264,6 @@ export default {
     onFileSizeExceed () {
       alert('File size exceeds. Please choose a file smaller than 100kb.')
     }
-  }
 
 }
 </script>
@@ -209,5 +294,18 @@ export default {
 }
 .croppa-container:hover {
   cursor: pointer;
+}
+.wrapper {
+  position: relative;
+  display: inline-block;
+  font-size: 0;
+}
+.icon-resize {
+  position: absolute;
+  right: 4px;
+  bottom: 4px;
+  font-size: 16px;
+  width: 2em;
+  cursor: nwse-resize;
 }
 </style>
